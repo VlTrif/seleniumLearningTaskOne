575c6e2a87a062dc2f221d02800f16542bffca32
Vladimir <trifonov.vladimir@physics.msu.ru>
2018-01-21 16:16:56 +0300

    Finished 1 task :)

--- src/test/java/task1/TaskOneClass.java
+++ src/test/java/task1/TaskOneClass.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -38,30 +39,82 @@ public class TaskOneClass {
         WebElement title = driver.findElement(By.xpath("//ul[contains(@aria-labelledby,'alt-menu-mid__header4')]//*[contains(text(),'Страхование путешественников')]"));
         wait.until(ExpectedConditions.visibilityOf(title));
         driver.findElement(By.xpath("//ul[contains(@aria-labelledby,'alt-menu-mid__header4')]//*[contains(text(),'Страхование путешественников')]")).click();
-        //Wait<WebDriver> wait = new WebDriverWait(driver, 10, 1000);
-        //assertEquals("Страхование путешественников",
-        //driver.findElement(By.xpath("//div[@class='sbrf-rich-outer']/h1")).getText());
 
-        /*WebElement title = driver.findElement(By.xpath("//div[@class='sbrf-rich-outer']/h1"));
-        wait.until(ExpectedConditions.visibilityOf(title));
-        assertEquals("Страхование путешественников", title.getText());*/
+        WebElement title2 = driver.findElement(By.xpath("//div[@class='sbrf-rich-outer']/h1"));
+        wait.until(ExpectedConditions.visibilityOf(title2));
+        assertEquals("Страхование путешественников", title2.getText());
 
         driver.findElement(By.xpath("//*[contains(@class, 'sbrf-rich-outer')]/p/a[contains(@target, '_blank')]/img")).click();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.navigate().forward();
 
-        //driver.findElement(By.xpath("//*[contains(@class,'b-form-prog-box')]//*[contains(text(),'Минимальная')]")).click();
-        //driver.findElement(By.xpath("//div[contains(@class,'b-form-prog-box')]//*[contains(text(),'Необходимый минимум для оплаты медицинской помощи за границей')]")).click();
-        WebElement selectElem = driver.findElement(By.xpath("//div[contains(@class,'b-form-prog-box')]"));
-                //tagName("select"));
-        Select select = new Select(selectElem);
-        //select.selectByVisibleText("Необходимый минимум для оплаты медицинской помощи за границей");
-        select.selectByValue("Минимальная");
+        ArrayList tabs2 = new ArrayList(driver.getWindowHandles());
+        driver.switchTo().window((String) tabs2.get(1));
 
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(
+                By.xpath("//*[contains(@ng-class,'b-form-prog-box')]//*[contains(text(),'Минимальная')]/..")))).click();
+
+        driver.findElement(By.xpath("//*[contains(@class,'b-button-block-center')]//*[contains(@class,'b-continue-btn')]")).click();
+
+        fillField(By.name("insured0_surname"),"Ivanov");
+        fillField(By.name("insured0_name"),"Ivan");
+        fillField(By.name("insured0_birthDate"),"21.01.1990");
+        fillField(By.name("surname"),"Иванов");
+        fillField(By.name("name"),"Иван");
+        fillField(By.name("middlename"),"Иванович");
+        driver.findElement(By.xpath("//*[@name='birthDate']")).click();
+        fillField(By.name("birthDate"),"21.01.1990");
+        fillField(By.name("passport_series"),"1122");
+        fillField(By.name("passport_number"),"222333");
+        fillField(By.name("issueDate"),"21.01.1999");
+        fillField(By.name("issuePlace"),"Отделением УФМС по г.Москва");
+
+        driver.findElement(By.xpath("//*[contains(text(),'Продолжить')]")).click();
+
+        assertEquals("Заполнены не все обязательные поля",
+                driver.findElement(By.xpath("//div[contains(@ng-show,'tryNext && myForm.$invalid')]")).getText());
+
+        assertEquals("Ivanov",
+                driver.findElement(By.name("insured0_surname")).getAttribute("value"));
+
+        assertEquals("Ivan",
+                driver.findElement(By.name("insured0_name")).getAttribute("value"));
+
+        assertEquals("21.01.1990",
+                driver.findElement(By.name("insured0_birthDate")).getAttribute("value"));
+
+        assertEquals("Иванов",
+                driver.findElement(By.name("surname")).getAttribute("value"));
+
+        assertEquals("Иван",
+                driver.findElement(By.name("name")).getAttribute("value"));
+
+        assertEquals("Иванович",
+                driver.findElement(By.name("middlename")).getAttribute("value"));
+
+        assertEquals("21.01.1990",
+                driver.findElement(By.name("birthDate")).getAttribute("value"));
+
+        assertEquals("1122",
+                driver.findElement(By.name("passport_series")).getAttribute("value"));
+
+        assertEquals("222333",
+                driver.findElement(By.name("passport_number")).getAttribute("value"));
+
+        assertEquals("21.01.1999",
+                driver.findElement(By.name("issueDate")).getAttribute("value"));
+
+        assertEquals("Отделением УФМС по г.Москва",
+                driver.findElement(By.name("issuePlace")).getAttribute("value"));
     }
+
+    private void fillField(By locator, String value) {
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(value);
+    }
+
     @After
     public void AfterTask(){
-        //driver.quit();
+        driver.quit();
 
     }
 }
